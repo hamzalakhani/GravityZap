cdeceb8727e607eadf66a54e6ca775678c3589f2
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-12-01 15:14:29 -0500

    adding storyScene

---
+++ TestGame/StoryScene.h
@@ -0,0 +1,13 @@
+//
+//  StoryScene.h
+//  TestGame
+//
+//  Created by Hamza Lakhani on 2016-12-01.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface StoryScene : SKScene
+
+@end
